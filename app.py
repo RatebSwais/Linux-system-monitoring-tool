aeee3c72be68dd05ed92fe04b2d850e603d7648c
Rateb <rateb@localhost.localdomain>
2019-01-16 16:57:50 +0200

    second commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import psutil   
 import math
+from bs4 import BeautifulSoup
 from flask import Flask, url_for, render_template
 
 #Instantiate app
@@ -19,6 +20,18 @@ memory = {
     'Cached': round(mem.cached / (1024.0 ** 3), 2),
     'Shared': round(mem.shared  / (1024.0 ** 3), 2),
 }
+
+memory_mb = {
+    'Total': round(mem.total / (1024.0 ** 2), 2),
+    'Used': round(mem.used / (1024.0 ** 2), 2),
+    'Available': round(mem.available / (1024.0 ** 2), 2),
+    'Percentage': round(mem.percent, 2),
+    'Free' : round(mem.free / (1024.0 ** 2), 2),
+    'Buffers': round(mem.buffers / (1024.0 ** 2), 2),
+    'Cached': round(mem.cached / (1024.0 ** 2), 2),
+    'Shared': round(mem.shared  / (1024.0 ** 2), 2),
+
+}
 #Fetch swap info
 swp = psutil.swap_memory()
 #Create dicitionary of swap memory info, change to GB and round to 2 digits
@@ -28,16 +41,44 @@ swap = {
     'Free' : round(swp.free / (1024.0 ** 3), 2),
     'Percentage': round(swp.percent, 2),
 }
+
+swap_mb = {
+    'Total': round(swp.total / (1024.0 ** 2), 2),
+    'Used': round(swp.used / (1024.0 ** 2), 2),
+    'Free' : round(swp.free / (1024.0 ** 2), 2),
+    'Percentage': round(swp.percent, 2),
+}
 #Fetch CPU info
 c = psutil.cpu_percent(interval=1, percpu=True)
 cpucount = psutil.cpu_count()
-print cpucount
-
+cpurange = range(cpucount)
+cpu = dict()
 #CPU dictionary
-cpu = {}
+for i in cpurange:
+    cpu[i] = c[i]
 
+#Fetch cpu frequency
+freq = psutil.cpu_freq()
+#Frequency dictionary
+frequency = {
+    'Current': round(freq.current, 2),
+    'Min': freq.min,
+    'Max': freq.max,
+}
+#Disk usage
+dusg = psutil.disk_usage('/')
 
+disk_usage = {
+    'Total': round(dusg.total / (1024.0 ** 3), 2),
+    'Used': round(dusg.used / (1024.0 ** 3), 2),
+    'Free': round(dusg.free / (1024.0 ** 3), 2),
+    'Percentage': dusg.percent,
+}
+
+#Disk partitions
+partis = psutil.disk_partitions(all=False)
+print partis
 #App routes
 @app.route('/')
 def proc():
-    return render_template('index.html', memory=memory, swap=swap)
+    return render_template('index.html', memory=memory, memory_mb=memory_mb, swap=swap, swap_mb=swap_mb, c=c, cpurange=cpurange, cpucount=cpucount, cpu=cpu, frequency=frequency, disk_usage=disk_usage)
